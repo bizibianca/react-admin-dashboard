3ae7c7ad7864c3c6f029dbb047a433560ab923d8
bizibianca <114431062+bizibianca@users.noreply.github.com>
2023-10-16 10:32:45 -0300

    context added, sidebar component updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,9 +5,18 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
 import { links } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { currentColor, activeMenu, setActiveMenu, screenSize } =
+    useStateContext();
+
+  const handleCloseSideBar = () => {
+    if (activeMenu !== undefined && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
+
   const activeLink =
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink =
@@ -19,31 +28,34 @@ const Sidebar = () => {
         <>
           <div className="flex items-center justify-between">
             <Link
-              className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text:white text-slate-900"
               to="/"
-              onClick={() => {}}
+              onClick={handleCloseSideBar}
+              className="flex items-center gap-3 mt-4 ml-3 text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu(!activeMenu)}
+                style={{ color: currentColor }}
                 className="block p-3 mt-4 text-xl rounded-full hover:bg-light-gray md:hidden"
               >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
           </div>
-          <div className="mt-10">
+          <div className="mt-10 ">
             {links.map((item) => (
               <div key={item.title}>
-                <p className="m-3 mt-4 uppercase text-gray"> {item.title}</p>
+                <p className="m-3 mt-4 text-gray-400 uppercase dark:text-gray-400">
+                  {item.title}
+                </p>
                 {item.links.map((link) => (
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={''}
+                    onClick={handleCloseSideBar}
                     style={({ isActive }) => ({
                       backgroundColor: isActive ? currentColor : '',
                     })}
