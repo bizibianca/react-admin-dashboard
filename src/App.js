a377df5699fc136ef1a78e68b61882080ee1c008
bizibianca <114431062+bizibianca@users.noreply.github.com>
2023-10-16 05:58:23 -0300

    first commit
    Components, pages and others components created, menu basic structure created

---
+++ src/App.js
@@ -0,0 +1,96 @@
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from './pages';
+import './App.css';
+
+const App = () => {
+  const activeMenu = true;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="relative flex dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 style={{zIndex: '1000">
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="p-3 text-3xl text-white hover:drop-shadow-xl hover:bg-light-gray"
+                style={{ background: 'blue', borderRadious: '50%' }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="fixed bg-white w-72 sidebar dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              <Sidebar />
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-72' : 'flex-2'
+            }`}
+          >
+            <div className="w-full fixe md:static bg-main-bg dark:bg-main0dark-bg navbar">
+              <Navbar />
+            </div>
+          </div>
+          <div>
+            <Routes>
+              {/* Dashboard */}
+              <Route path="/" element={<Ecommerce />} />
+              <Route path="/ecommerce" element={<Ecommerce />} />
+
+              {/* Pages */}
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/employees" element={<Employees />} />
+              <Route path="/customers" element={<Customers />} />
+
+              {/* Apps */}
+              <Route path="/kanban" element={<Kanban />} />
+              <Route path="/editor" element={<Editor />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/color-picker" element={<ColorPicker />} />
+
+              {/* Charts */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/area" element={<Area />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/financial" element={<Financial />} />
+              <Route path="/color-mapping" element={<ColorMapping />} />
+              <Route path="/pyramid" element={<Pyramid />} />
+              <Route path="/stacked" element={<Stacked />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
